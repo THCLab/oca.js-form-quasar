4fccd5449945565a04886e00f8f6f077952b8a78
Marcin Olichwiruk <marcin@opensoftware.pl>
2021-11-12 12:06:17 +0100

    Add Checkbox Control

--- ui/src/components/Form/Control/Checkbox.vue
+++ ui/src/components/Form/Control/Checkbox.vue
@@ -1,14 +1,43 @@
 <template>
-  <div>
-    checkbox
-  </div>
+  <q-checkbox
+    :label="translations[formMeta.language].label"
+    :disable="formMeta.readonly"
+    v-model="value"
+    dense
+    left-label />
+
+  <slot name="information"/>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  name: 'CheckboxControl'
+  name: 'CheckboxControl',
+  props: {
+    control: {
+      type: Object,
+      required: true,
+      validator(value) {
+        if (!value.hasOwnProperty('name')) { return false }
+        if (!value.hasOwnProperty('translations')) { return false }
+        return true
+      }
+    },
+    formMeta: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    const translations = JSON.parse(JSON.stringify(props.control.translations))
+    const value = ref(false)
+
+    return {
+      translations,
+      value
+    }
+  }
 })
 </script>
 
