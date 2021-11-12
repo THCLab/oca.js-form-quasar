535091616acea13caa67827a44b3aa1b915b2d5e
Marcin Olichwiruk <marcin@opensoftware.pl>
2021-11-12 14:42:35 +0100

    Add Number Control

--- ui/src/components/Form/Control/Number.vue
+++ ui/src/components/Form/Control/Number.vue
@@ -1,14 +1,43 @@
 <template>
-  <div>
-    number
-  </div>
+  <q-input
+      type="number"
+      v-model="value"
+      :label="translations[formMeta.language].label"
+      :readonly="formMeta.readonly"
+      dense
+      outlined />
+  <slot name="information"/>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  name: 'NumberControl'
+  name: 'NumberControl',
+  props: {
+    control: {
+      type: Object,
+      required: true,
+      validator(value) {
+        if (!value.hasOwnProperty('name')) { return false }
+        if (!value.hasOwnProperty('translations')) { return false }
+        return true
+      }
+    },
+    formMeta: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    const translations = JSON.parse(JSON.stringify(props.control.translations))
+    const value = ref()
+
+    return {
+      translations,
+      value
+    }
+  }
 })
 </script>
 
